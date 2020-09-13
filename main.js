8d694979e1cbe202ad1c0933a2441083cbed36e1
zmax101 <69588727+zmax101@users.noreply.github.com>
2020-09-13 08:04:43 +0300

    Перелапатил блок Ultimate + адаптив

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
@@ -12,3 +12,23 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+$('ul.menu a[href^="#"').click(function () {
+  let target = $(this).attr('href');
+  $('html, body').animate({
+    scrollTop: $(target).offset().top
+  }, 500);
+  $('ul.menu a[href^="#"').css({ 'color': '#212121' });
+  $(this).css({ 'color': '#004bee' });
+  return false;
+})
+$('.menu-icon').click(function () {
+  $('nav').slideToggle(500);
+  $('ul.menu').css({
+    'display': 'flex', 'flex-direction': 'column'
+  })
+  if ($('.menu-icon').html() == '<i class="fas fa-bars"></i>') {
+    $(this).html('<i class="fas fa-times"></i>')
+  } else {
+    $(this).html('<i class="fas fa-bars"></i>')
+  }
+})
